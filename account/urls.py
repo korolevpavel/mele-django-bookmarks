079aca33b756e739ed74da988faae46461a1996f
Korolev Pavel <korolev.vrn@gmail.com>
2020-08-07 23:26:47 +0300

    add custom register form

--- account/urls.py
+++ account/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
          name='password_reset_confirm'),
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete')
+         name='password_reset_complete'),
+
+    #register
+    path('register/', views.register, name='register')
 
 ]
