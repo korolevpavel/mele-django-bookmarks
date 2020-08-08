54d5c38a90559101df5bb5e01a48ee2ae9c2cd39
Korolev Pavel <korolev.vrn@gmail.com>
2020-08-08 22:42:02 +0300

    add user profile

--- account/urls.py
+++ account/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
 
-    #register
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 
 ]
