ed8487315d483c9cb8bf03a8047e39dc42cfa13d
Korolev Pavel <korolev.vrn@gmail.com>
2020-08-15 22:35:19 +0300

    add authentication with email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
